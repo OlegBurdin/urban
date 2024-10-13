bf40a6b019cea25f07797256e01d3d136befed77
OlegBurdin <oburdin72@gmail.com>
2024-10-13 15:27:40 +0300

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -6,3 +6,27 @@ def divide(first, second):
     else:
         return (first / second)
 
+def divide(first, second):
+    if second == 0:
+       return ('Ошибка')
+    else:
+       return (first/second)
+
+
+from fake_math import divide as div_1
+from true_math import divide as div_2
+
+result1 = div_1(69, 3)
+result2 = div_1(3, 0)
+result3 = div_2(49, 7)
+result4 = div_2(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+>
+23.0
+Ошибка
+7.0
+inf
