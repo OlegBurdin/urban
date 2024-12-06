a27822f299c0aaca19e82ce03cd055f19b60e1b0
OlegBurdin <oburdin72@gmail.com>
2024-12-06 15:10:54 +0300

    Create module_9_4.py

---
+++ module_9_4.py
@@ -0,0 +1,37 @@
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+res = list(map(lambda x, y: x == y, first, second))
+print(res)
+def get_advanced_writer(file_name):
+
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for data in data_set:
+                file.write(str(data) + '\n')
+    return write_everything
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+import random
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return random.choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
+=>
+[False, True, True, False, False, False, False, False, True, False, False, False, False, False]
+Да
+Наверное
+Нет
+
+Process finished with exit code 0
+example.txt
+=>
+Это строчка
+['А', 'это', 'уже', 'число', 5, 'в', 'списке']
